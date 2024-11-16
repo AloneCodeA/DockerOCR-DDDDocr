6d0b4382e63c7944db5a29d4e6995bf25af9734d
AloneCodeA <119469048+AloneCodeA@users.noreply.github.com>
2024-11-16 19:34:15 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from NewOcr import app
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",1337))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.getenv('PORT', 1337)), debug=True)
